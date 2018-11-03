1e741f2f3e94ed4a6cec381d9bc6772555c92c78
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2018-11-04 00:22:13 +0200

    Finished Configure & Routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,19 +2,16 @@ import Vapor
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
-    // Basic "It works" example
-    router.get { req in
-        return "It works!"
-    }
     
-    // Basic "Hello, world!" example
-    router.get("hello") { req in
-        return "Hello, world!"
-    }
+    let userController = UserController()
+    
+    router.get("users", use: userController.list)
+    router.post("users", use: userController.create)
+    router.patch("users", User.parameter, use: userController.update)
+    router.delete("users", User.parameter, use: userController.delete)
+    
+    let intoleranceController = IntoleranceController()
+    
+    router.post("intolerances", use: intoleranceController.create)
 
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
