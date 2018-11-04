3c2c16fbbf96b041bed6ca5ac7a93b29a98deab6
Alexandru Turcanu <alexandru_turcanu@ymail.com>
2018-11-04 08:05:09 +0200

    Added CreationDate for User

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -11,10 +11,12 @@ import Vapor
 final class User: SQLiteModel {
     var id: Int?
     var username: String
+    var creationDate: Date
     
     init(id: Int? = nil, username: String) {
         self.id = id
         self.username = username
+        self.creationDate = Date()
     }
 }
 
